62f2af5cfd6ac522018416778c6a32494dfe9920
vwaizer <lightwing2208@gmail.com>
2023-08-18 09:16:18 +0700

    fix detail

--- public/js/basket/detail.js
+++ public/js/basket/detail.js
@@ -1,21 +1,32 @@
-import { productData } from "/finalProject/public/js/basket/data.js";
+import { productData } from "../data.js";
+import { clickFunction } from "./detail2.js";
 
-
-let newUrl= window.location;
-console.log(newUrl);
+let newUrl= window.location.href;
 let url = new URL(newUrl);
 let search_param = url.searchParams;
 if(search_param.has("id")){
     // console.log(search_param.get("id"));
-    const newId = Number(search_param.get("id"))
+    const newId = parseInt(search_param.get("id"))
     console.log(newId);
     const newData = productData.find(({ id }) => id === newId )
     console.log(newData);
-    document.getElementById("leftbox").innerHTML=newData
+    document.getElementById("leftbox").innerHTML=`
+     <div >
+          <div class="d-flex flex-column justify-content-between p-3">
+            <div class="p-2">${newData.nameProduct}</div>
+            <div class="p-2"><img src="${newData.picture}"> </div>
+            <div class="p-2">${newData.price}</div>
+          </div>
+        </div> 
+    `
 }
 
-
-
+// function clickButton(){
+//     window.location.href="http://127.0.0.1:5500/basket.html?id=${idItem}"
+// }
+document.getElementById("click").addEventListener("click", function () {
+    clickFunction(search_param.get("id"))
+})
 function isItem(){
     let takeItem = localStorage.getItem("information")
     console.log(takeItem);
--- public/js/basket/detail2.js
+++ public/js/basket/detail2.js
@@ -2,4 +2,7 @@
 // import { isItem } from "/finalProject/public/js/basket/detail2.js";
 
 // isItem()
+export function clickFunction(idItem){
+    window.location.href=`http://127.0.0.1:5500/basket.html?id=${idItem}`
+}
 
